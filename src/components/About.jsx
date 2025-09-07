59aa659b23e36a1c11eb9ad0c286085cada10c4c
ruwuuu <ruel.sinha.can@gmail.com>
2025-09-06 20:46:01 -0700

    3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -93,7 +93,7 @@ export default memo(function About() {
 
 					<div className="about-subtitle">
 						<GraduationCap className="w-4 h-4" />
-						<span>IIT (BHU) Varanasi</span>
+						<span>IVancouver Technical</span>
 					</div>
 
 					<p className="about-description">
